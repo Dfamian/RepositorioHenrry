d8776495af33c8ce6b231152d13eaa1d2e057131
Damian <gabrielvillotabenavides@gmail.com>
2024-03-08 16:13:40 -0500

    adelanto de la clase de javascript , operadores y variables

---
+++ C5_javascript/operadores.js
@@ -0,0 +1,18 @@
+console.log(3+5); // operador suma
+console.log(7-2); // operador resta 
+console.log(5/2); // operador dividion
+console.log(3*9); // operador multiplicacion
+
+console.log(95 % 4); // operador division cociente resto
+console.log(95/4);
+//-------------------------------------------------------------------------------------------------------//
+
+console.log( 3 + 5 * 2 - 8); //regla de precedencia es 5 , primero multiplicacion y luego , suma y resta 
+console.log((3+5) * 2 - 8 ); // se suma el parentesis , este resultado se multiplica por 2 y se resta por 8
+
+//'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+
+console.log('hola' + ' juan')
+var nombre = 'Pedrito'
+var saludo = 'Hola ' + nombre
+console.log(saludo);
---
+++ C5_javascript/variables.js
@@ -0,0 +1,22 @@
+//palabras o strings
+
+var cajon1 = 'bifanda verde';
+var bootcamp = 'Henrrt';
+//numeros
+
+var numeroEntero = 17;
+var numeroNegativo = -12;
+var numeroDecimal = 11.65;
+//boleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+//Undefine
+
+var Cajon2;
+console.log(Cajon2);
+
+//null
+
+var sinNada = null;
