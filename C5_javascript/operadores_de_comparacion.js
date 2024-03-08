78795295469927302d4261dc06920ee222654903
Damian <gabrielvillotabenavides@gmail.com>
2024-03-08 18:07:11 -0500

    operadores de comparacion y asicuatividad

---
+++ C5_javascript/operadores_de_comparacion.js
@@ -0,0 +1,17 @@
+console.log(4 < 7);
+console.log(4 < 1);
+console.log(4 > 4);
+console.log(4 == 7);
+
+// diferencias entre == y ===//
+console.log('DIFERENCIA ENTRE == Y ===');
+console.log(3 == 3);
+console.log(3 == '3');
+console.log (3 === '3');
+// ASOCIATIVIDAD 
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log(c);
+
+console.log(16/2/4); //la division tiene una asociatividad a la izquierda
