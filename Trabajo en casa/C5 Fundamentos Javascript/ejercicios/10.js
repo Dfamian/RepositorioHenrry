6a0d70055e74ff04b9fd1f0e81346cd75044f37a
Damian <gabrielvillotabenavides@gmail.com>
2024-03-14 18:57:10 -0500

    añadiendo la carpeta trabajo en casa pero sin un .git dentro

---
+++ Trabajo en casa/C5 Fundamentos Javascript/ejercicios/10.js
@@ -0,0 +1,16 @@
+function esFechaValida(fecha) {
+  // La función recibe un argumento "fecha".
+  // Comprueba si este corresponde a una fecha válida.
+  // Si es así, retorna true, sino retorna false.
+  // Tu código:
+  var verFecha = new Date(fecha);
+
+  if (typeof(verFecha) === Date){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+module.exports = esFechaValida;
---
+++ Trabajo en casa/C5 Fundamentos Javascript/ejercicios/11.js
@@ -0,0 +1,20 @@
+function esMayorDeEdad(fechaNacimiento) {
+  // La función recibe un argumento "fechaNacimiento" correspondiente a la fecha
+  // de nacimiento de una persona.
+  // Determina si esta persona es mayor de edad (tiene 18 años o más).
+  // Devuelve true si lo es, caso contrario, retorna false.
+  // PISTA: Investiga el método getFullYear() de la clase Date para obtener el año actual.
+  // Tu código:
+  var fechaNac = new Date(fechaNacimiento);
+  var añoDenaC = fechaNac.getFullYear();
+  var añoActual = new Date().getFullYear();
+
+if (añoActual - añoDenaC >= 18){
+  return true;
+}
+else {
+  return false;
+}
+}
+
+module.exports = esMayorDeEdad;
