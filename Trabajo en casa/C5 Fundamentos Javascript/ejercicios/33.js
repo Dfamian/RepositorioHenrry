01ef1c74d9f9e1c56ff2c4e51f03489b7b1472c5
Damian pc escritorio <gabrielvillotabenavides@gmail.com>
2024-03-22 19:31:04 -0500

    terminando los ejercicios del 33 a 39

--- Trabajo en casa/C5 Fundamentos Javascript/ejercicios/33.js
+++ Trabajo en casa/C5 Fundamentos Javascript/ejercicios/33.js
@@ -6,7 +6,13 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
-  
+  var division = (numero / numero);
+  if (division == 0, numero / 1 == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 module.exports = esNumeroPrimo;
