5189afad582620ca9d3630e7c60e8b2486925df8
Damian pc escritorio <gabrielvillotabenavides@gmail.com>
2024-03-25 11:48:52 -0500

    cambios muy peque

--- C5_javascript/bucles/explicaciones en clase/demo1.js
+++ C5_javascript/bucles/explicaciones en clase/demo1.js
@@ -3,46 +3,46 @@
 //}
 
 
-//function monstrarNumeros(Limite){
- //   for (var i = 0; i <= Limite ; i++)
+// function monstrarNumeros(Limite){
+//    for (var i = 0; i <= Limite ; i+=20)
 //    console.log(i)
-//}
-//monstrarNumeros(800)
+// }
+// monstrarNumeros(800)
 
 // function esNUmeroPrimo(numero){
-//     //la funcion recibe un argumento "numero"
-//     //determina si este corresponde a un numero primo.
-//     //De ser asi , retorna true
-//     //De lo contrario retorna flase
-//     //IMPORTANTE recuerda que el numero primo es aquel que
-//     //solo es divisible por si mosmo y por 1 
-//     //Tu codigo :
-//     for (var i = 2 ; i < numero ; i++){
-//         if ( numero % 1 === 0){
+    //la funcion recibe un argumento "numero"
+    //determina si este corresponde a un numero primo.
+    //De ser asi , retorna true
+    //De lo contrario retorna flase
+    //IMPORTANTE recuerda que el numero primo es aquel que
+    //solo es divisible por si mosmo y por 1 
+    //Tu codigo :
+//    for (var i = 2 ; i < numero ; i++){
+//       if ( numero % 1 === 0){
 //             return false
-//     }
-//         else {
+//    }
+//       else {
 //             return true
-//         }
-//     }
+//       }
+//    }
 // }
 
 
-var color = "azul";
+// var color = "azul";
 
-switch(color){
-    case "azul":
-    console.log("el color es azul")
-    break
-    case "rojo":
-    console.log("el color es rojo")
-    break
-    case "amarillo":
-    console.log("el color es amarillo")
-    break
-    case "verde":
-    console.log("el color es verde")
-    break
+// switch(color){
+//     case "azul":
+//     console.log("el color es azul")
+//     break
+//     case "rojo":
+//     console.log("el color es rojo")
+//     break
+//     case "amarillo":
+//     console.log("el color es amarillo")
+//     break
+//     case "verde":
+//     console.log("el color es verde")
+//     break
 
-    default: console.log("no existe el color")
-}
+//     default: console.log("no existe el color")
+// }
