73f96a64456541e75ad8d7ef7a3959526123affb
Damian pc escritorio <gabrielvillotabenavides@gmail.com>
2024-03-25 11:30:52 -0500

    agregando ejemplos del for y whle

---
+++ C5_javascript/bucles/bucles for y while.js
@@ -0,0 +1,14 @@
+var suma = 0
+
+// for(var i=0 ; i<5; i++ ){
+//     suma = suma + i
+//     console.log(suma)
+//     console.log("variable de iteracion",i)
+// }
+// console.log("Variable suma",suma)
+
+
+// while(suma < 3){
+//     suma = suma + 1;
+//     console.log(suma);
+// }
