a9776d35d7746b9b8308ffdcb28c7f4d50216570
Damian pc escritorio <gabrielvillotabenavides@gmail.com>
2024-03-25 11:03:39 -0500

    creacion de notas globales.js y adicion de ejrcicios para ejemplos de operadores logicos

--- C5_javascript/bucles/explicaciones en clase/demo1.js
+++ C5_javascript/bucles/explicaciones en clase/demo1.js
@@ -9,20 +9,40 @@
 //}
 //monstrarNumeros(800)
 
-function esNUmeroPrimo(numero){
-    //la funcion recibe un argumento "numero"
-    //determina si este corresponde a un numero primo.
-    //De ser asi , retorna true
-    //De lo contrario retorna flase
-    //IMPORTANTE recuerda que el numero primo es aquel que
-    //solo es divisible por si mosmo y por 1 
-    //Tu codigo :
-    for (var i = 2 ; i < numero ; i++){
-        if ( numero % 1 === 0){
-            return false
-        }
-        else {
-            return true
-        }
-    }
+// function esNUmeroPrimo(numero){
+//     //la funcion recibe un argumento "numero"
+//     //determina si este corresponde a un numero primo.
+//     //De ser asi , retorna true
+//     //De lo contrario retorna flase
+//     //IMPORTANTE recuerda que el numero primo es aquel que
+//     //solo es divisible por si mosmo y por 1 
+//     //Tu codigo :
+//     for (var i = 2 ; i < numero ; i++){
+//         if ( numero % 1 === 0){
+//             return false
+//     }
+//         else {
+//             return true
+//         }
+//     }
+// }
+
+
+var color = "azul";
+
+switch(color){
+    case "azul":
+    console.log("el color es azul")
+    break
+    case "rojo":
+    console.log("el color es rojo")
+    break
+    case "amarillo":
+    console.log("el color es amarillo")
+    break
+    case "verde":
+    console.log("el color es verde")
+    break
+
+    default: console.log("no existe el color")
 }
---
+++ C5_javascript/bucles/operadores logicos.js
@@ -0,0 +1,30 @@
+// function mayorMenorYPar(num){
+//     if (num > 5 && num  < 10 && num % 2 === 0)console.log(true);
+//     else console.log(false);
+// }
+
+// mayorMenorYPar(8)
+
+// function operadorOr (str) {
+// if( str == "Henrry" || str.length < 2) console.log(true) ;
+// else console.log(false)
+// }
+
+// operadorOr("x")
+
+// function Negacion (permiso){
+//     if (!permiso === true) console.log("Tiene permiso")
+    
+// }
+// Negacion(true)
+// Negacion(false)
+
+function condicionCompleja (num){
+    if ( num > 9 && num % 2 === 0 || num === 3 ) console.log(true);
+    else console.log(false);
+}
+condicionCompleja(10);
+condicionCompleja(6);
+condicionCompleja(3);
+condicionCompleja(5);
+
