08bed394770b1bdd121ea1e0dddb33c4061a51de
Damian pc escritorio <gabrielvillotabenavides@gmail.com>
2024-04-09 17:15:38 -0500

    agregando archivos

--- Trabajo en casa/C7 Arrays/ejercicios/06.js
+++ Trabajo en casa/C7 Arrays/ejercicios/06.js
@@ -1,6 +1,10 @@
 function invertirArray(array) {
   // Invierte el arreglo array recibido por argumento.
   // Tu código:
+  var Invertir = array.split("")
+  Invertir = Invertir.reverse(array)
+  Invertir = Invertir.join("")
+  console.log(Invertir)
+  return Invertir
 }
-
 module.exports = invertirArray;
--- Trabajo en casa/C7 Arrays/ejercicios/07.js
+++ Trabajo en casa/C7 Arrays/ejercicios/07.js
@@ -2,6 +2,20 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
-}
+  
+  //comparar valores tipo string
+
+  array.sort((ItemA , ItemB) => {
+    console.log(ItemA.localeCompare(ItemB))
+    return ItemA.LocaleCompare(ItemB)
+  })
+// comparar valores numericos
+array.sort((ItemA , ItemB) => {
+  console.log(ItemA - ItemB)
+  return ItemA - ItemB
+})
+} 
+ordenarArray  ("cafe", "discord")
+ordenarArray  (1,2,3,4,5)
 
 module.exports = ordenarArray;
